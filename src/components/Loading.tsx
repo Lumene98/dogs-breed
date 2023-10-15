f7cc03a3a5d0d9344160f0ffd063d4328b202f95
lumene <lucamene98@outlook.it>
2023-10-15 18:47:31 +0200

    Improve loading colors

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 export const Loading = () => {
   return (
-    <div className="fixed bottom-4 flex h-6 w-6 items-center justify-center rounded-2xl bg-white">
-      <span className="animate-spin font-bold text-gray-700 after:content-['.']"></span>
+    <div className="fixed bottom-4 flex h-6 w-6 items-center justify-center rounded-2xl bg-slate-400">
+      <span className="animate-spin font-bold text-white after:content-['.']"></span>
     </div>
   );
 };
