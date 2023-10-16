491dee6255d250597424a7df430556294b00107f
Luca Meneghini <lucamene98@outlook.it>
2023-10-16 17:08:51 +0200

    Hide border when chip selected

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -1,7 +1,7 @@
 const Chip = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
     <button
-      className="whitespace-nowrap rounded-xl border border-slate-600 px-3 py-1 text-lg font-medium capitalize text-slate-200 aria-selected:bg-indigo-800"
+      className="whitespace-nowrap rounded-xl border border-slate-600 px-3 py-1 text-lg font-medium capitalize text-slate-200 aria-selected:border-0 aria-selected:bg-indigo-800"
       {...props}
     />
   );
