76c2f46da069e7cd6a3cb9168d4064234b5c93c1
Luca Meneghini <lucamene98@outlook.it>
2023-10-16 14:32:37 +0200

    Improve loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="fixed bottom-4 flex h-6 w-6 items-center justify-center rounded-2xl bg-slate-400">
+    <div className="fixed left-1/2 top-4 flex h-10 w-10 items-center justify-center rounded-2xl bg-slate-600">
       <span className="animate-spin font-bold text-white after:content-['.']"></span>
     </div>
   );
