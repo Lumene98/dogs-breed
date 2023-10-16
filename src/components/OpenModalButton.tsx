6216f7089abb0ec34e0f529fff5bbdb80f6783a7
Luca Meneghini <lucamene98@outlook.it>
2023-10-16 17:12:28 +0200

    Align button

--- src/components/OpenModalButton.tsx
+++ src/components/OpenModalButton.tsx
@@ -5,7 +5,7 @@ interface IOpenModalButtonProps {
 const OpenModalButton = (props: IOpenModalButtonProps) => {
   return (
     <button
-      className="filter-button fixed bottom-5 mx-2 rounded-xl bg-indigo-500 py-4 font-semibold hover:bg-indigo-400"
+      className="filter-button fixed bottom-5 rounded-xl bg-indigo-500 py-4 font-semibold hover:bg-indigo-400"
       onClick={props.onClick}
     >
       Filter
